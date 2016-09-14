0da558ac8dfed09e8ddab8698bc0586728e15eb6
kgrashad <kgrashad@live.com>
2016-09-15 00:53:22 +0200

    Change the echo action to post an echo message to the connector API

--- echo_bot/mix.exs
+++ echo_bot/mix.exs
@@ -17,7 +17,7 @@ defmodule EchoBot.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {EchoBot, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :httpotion]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,6 +33,7 @@ defmodule EchoBot.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:httpotion, "~> 3.0.0"}]
   end
 end
--- echo_bot/web/controllers/echo_controller.ex
+++ echo_bot/web/controllers/echo_controller.ex
@@ -1,18 +1,33 @@
 defmodule EchoBot.EchoController do
   use EchoBot.Web, :controller
-  alias EchoBot.Models.Message
 
   def echo(conn, params) do
     get_message(conn, params)
   end
 
-  defp get_message(conn, %{"message" => msg}) do
-    json(conn, %{ echoed_message: msg})
+  defp get_message(conn, %{ "serviceUrl" => serviceUrl, "text" => text, "from" => from, "conversation" => conversation, "recipient" => recipient}) do
+    # Create the echo response and post it to the connector API
+    # Eventually this should be moved to the elixir-bot-builder and just exposed here as bot.send
+    # TODO: Should we do the post async without waiting for it?
+    # TODO: How to handler failures in encoding or posting? Right now we are raising exceptions (!)
+    url = Enum.join([serviceUrl, "v3/conversations/", conversation["id"], "/activities"], "")
+    body = Poison.encode!(%{
+      type: "message",
+      from: %{ id: recipient["id"], name: recipient["name"]}, 
+      conversation: %{ id: conversation["id"], name: conversation["name"]},
+      recipient: %{ id: from["id"], name: from["name"]},
+      text: "You said: " <> text})
+
+    HTTPotion.post!(url, [body: body, headers: [{"Content-Type", "application/json"}]])
+    
+    conn 
+    |> put_status(202)
+    |> json(%{})
   end
 
   defp get_message(conn, _) do
     conn |>
     put_status(:bad_request) |>
-    json(%{ error: "Please specify the 'message' parameter"})
+    json(%{ error: "Invalid Request"})
   end
 end
